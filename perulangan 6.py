5d30d517f3882fe36401ff2c381ecc8da42f5278
yosefinabakhita93 <78463400+yosefinabakhita93@users.noreply.github.com>
2021-02-24 13:54:28 +0700

    Tugas Pemrograman Python 2

---
+++ perulangan 6.py
@@ -0,0 +1,9 @@
+kata = 'nama saya yosefina'
+ 
+jum = 0
+for letter in kata:
+    if letter == 's':
+        jum += 1
+        #continue
+    #print("huruf sekarang : ", letter)
+print('jumlah : ', jum)
